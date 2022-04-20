a83ca5879937fbcf00314e0d5640b975d1474b70
kamral010101@gmail.com <kamral010101@gmail.com>
2022-04-20 09:53:51 +0200

    rename project

--- yatube/yatube/asgi.py
+++ yatube/yatube/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for anfisa project.
+ASGI config for yatube project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anfisa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube.settings')
 
 application = get_asgi_application()
